ed36c52c03dfb4705fa14992ebfa24e3f5c1898e
Maxime Bailly <maxime3510@gmail.com>
2021-01-27 15:05:39 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
-import express from "express";
+import express, { Response, Request } from "express";
 import { BookManager } from "./bookManager";
+//import bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080; // default port to listen
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 const bookManager: BookManager = new BookManager();
 
-app.get( "/book", ( req: any, res: any ) => {
+app.get( "/book", ( req: Request, res: Response ) => {
     res.send(bookManager.findAll());
 });
 
-app.get( "/book/:id", ( req: any, res: any ) => {
-    res.send(bookManager.findById(req.param.id));
+app.get( "/book/:id", ( req: Request, res: Response ) => {
+    res.send(bookManager.findById(parseInt(req.params.id)));
 });
 
-app.delete("book/:id", ( req: any, res: any ) => {
-    res.send(bookManager.deleteById(req.params.id));
+app.delete("book/:id", ( req: Request, res: Response ) => {
+    res.send(bookManager.deleteById(parseInt(req.params.id)));
 });
 
-app.post('/book/new', ( req: any, res: any ) => {
-    res.send(bookManager.createOne(req.body));
+app.post('/book/new', ( req: Request, res: Response ) => {
+    console.log(req.body);
+    //res.send(bookManager.createOne(req.body));
 });
 
-app.post('/book/update/:id', ( req: any, res: any ) => {
-    res.send(bookManager.updateOne(req.params.id, req.body));
+app.post('/book/update/:id', ( req: Request, res: Response ) => {
+    res.send(bookManager.updateOne(parseInt(req.params.id), req.body));
 });
 
 // start the Express server
